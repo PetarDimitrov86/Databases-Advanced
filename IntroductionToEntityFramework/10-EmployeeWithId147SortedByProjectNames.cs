f58fd7ec83bd66b75cfcb2a04b22aac840b731b2
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-27 23:19:27 +0300

    Add files via upload

---
+++ IntroductionToEntityFramework/10-EmployeeWithId147SortedByProjectNames.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using testing1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new SoftUniContext())
+        {
+            var employee = context.Employees.Find(147);
+            Console.WriteLine($"{employee.FirstName} {employee.LastName} {employee.JobTitle}");
+            foreach (var project in employee.Projects.OrderBy(p => p.Name))
+            {
+                Console.WriteLine($"{project.Name}");
+            }
+        }
+    }
+}
