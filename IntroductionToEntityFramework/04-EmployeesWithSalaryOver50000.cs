caa1cf9dc6dd8bc0d86cb92b73e708fb51bbc6d8
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-27 14:36:21 +0300

    Add files via upload

---
+++ IntroductionToEntityFramework/04-EmployeesWithSalaryOver50000.cs
@@ -0,0 +1,17 @@
+﻿using _01_ImportTheSoftuniDatabase;
+using System;
+using System.Linq;
+public class EmployeesFullInformation
+{
+    static void Main(string[] args)
+    {
+        using (var context = new SoftuniContext())
+        {
+            var employees = context.Employees.Where(e => e.Salary > 50000);
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"{employee.FirstName}");
+            }
+        }
+    }
+}
