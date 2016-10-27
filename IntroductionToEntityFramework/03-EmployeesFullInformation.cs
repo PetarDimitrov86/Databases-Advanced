8a3b01c07ceb96cc706e426753e8b9e198d9a54e
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-27 14:30:12 +0300

    Add files via upload

---
+++ IntroductionToEntityFramework/03-EmployeesFullInformation.cs
@@ -0,0 +1,16 @@
+﻿using _01_ImportTheSoftuniDatabase;
+using System;
+public class EmployeesFullInformation
+{
+    static void Main(string[] args)
+    {
+        using (var context = new SoftuniContext())
+        {
+            var employees = context.Employees;
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary}");
+            }
+        }
+    }
+}
