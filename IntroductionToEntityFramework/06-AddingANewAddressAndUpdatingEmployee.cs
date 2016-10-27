a671919015954b2175caa1174d135daa73002433
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-27 15:01:10 +0300

    Add files via upload

---
+++ IntroductionToEntityFramework/06-AddingANewAddressAndUpdatingEmployee.cs
@@ -0,0 +1,28 @@
+﻿using _01_ImportTheSoftuniDatabase;
+using System;
+using System.Linq;
+public class EmployeesFullInformation
+{
+    static void Main(string[] args)
+    {
+        using (var context = new SoftuniContext())
+        {
+            var address = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownID = 4
+            };
+
+            Employee employee = null;
+            employee = context.Employees.FirstOrDefault(e => e.LastName == "Nakov");
+            employee.Address = address;
+
+            context.SaveChanges();
+            var employees = context.Employees.OrderByDescending(e => e.AddressID).Take(10);
+            foreach (var emp in employees)
+            {
+                Console.WriteLine($"{emp.Address.N}");
+            }
+        }
+    }
+}
