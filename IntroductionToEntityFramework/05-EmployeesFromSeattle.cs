180d0da58ccb5f4b596ad6a9af984d976d1e7ebf
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-27 14:48:57 +0300

    Add files via upload

---
+++ IntroductionToEntityFramework/05-EmployeesFromSeattle.cs
@@ -0,0 +1,17 @@
+﻿using _01_ImportTheSoftuniDatabase;
+using System;
+using System.Linq;
+public class EmployeesFullInformation
+{
+    static void Main(string[] args)
+    {
+        using (var context = new SoftuniContext())
+        {
+            var employees = context.Employees.Where(e => e.Department.Name == "Research and Development").OrderBy(e => e.Salary).ThenByDescending(e => e.FirstName);
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName} from {employee.Department.Name} - ${employee.Salary:f2}");
+            }
+        }
+    }
+}
