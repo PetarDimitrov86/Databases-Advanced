0ce4091dcf11142db9d170ddd136031ba9789a30
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-28 01:14:25 +0300

    Add files via upload

---
+++ IntroductionToEntityFramework/11-DepartmentsWithMoreThan5Employees.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using testing1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new SoftUniContext())
+        {
+            var departments = context.Departments.Where(x => x.Employees.Count > 5).OrderBy(x => x.Employees.Count);
+            foreach (var department in departments)
+            {
+                Console.WriteLine($"{department.Name} {context.Employees.FirstOrDefault(x => x.EmployeeID == department.ManagerID).FirstName}");
+                foreach (var employee in department.Employees)
+                {
+                    Console.WriteLine($"{employee.FirstName} {employee.LastName} {employee.JobTitle}");
+                }
+            }
+        }
+    }
+}
