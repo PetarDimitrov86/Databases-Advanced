b4d87678b385e5749063425344c4cd50d663e67c
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-28 00:09:35 +0300

    Add files via upload

---
+++ IntroductionToEntityFramework/09-AddressesByTownName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using testing1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new SoftUniContext())
+        {
+            var addresses = context.Addresses.OrderByDescending(x => x.Employees.Count)
+                .ThenBy(e => e.Town.Name)
+                .Take(10);
+            foreach (var address in addresses)
+            {
+                Console.WriteLine($"{address.AddressText}, {address.Town.Name} - {address.Employees.Count} employees");
+            }
+        }
+    }
+}
