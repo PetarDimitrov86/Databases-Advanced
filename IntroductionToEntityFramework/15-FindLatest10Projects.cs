b76a609016a23a626134c186a6af15de7dbfe9e7
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-28 01:22:43 +0300

    Add files via upload

---
+++ IntroductionToEntityFramework/15-FindLatest10Projects.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using testing1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new SoftUniContext())
+        {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InstalledUICulture;
+            var projects = context.Projects.OrderByDescending(p => p.StartDate).Take(10);
+            foreach (var project in projects.OrderBy(p => p.Name))
+            {
+                Console.WriteLine($"{project.Name} {project.Description} {project.StartDate} {project.EndDate}");
+            }
+        }
+    }
+}
