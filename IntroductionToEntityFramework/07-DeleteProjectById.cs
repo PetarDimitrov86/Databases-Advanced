219aa7b774b2a1e887e2b339d2ef7b9bea27ff63
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-27 15:15:07 +0300

    Add files via upload

---
+++ IntroductionToEntityFramework/07-DeleteProjectById.cs
@@ -0,0 +1,27 @@
+﻿using _01_ImportTheSoftuniDatabase;
+using System;
+using System.Linq;
+public class EmployeesFullInformation
+{
+    static void Main(string[] args)
+    {
+        using (var context = new SoftuniContext())
+        {
+            var project = context.Projects.Find(2);
+
+            var employeesToRemove = project.Employees.Where(e => e.Projects == project);
+            var employees = context.Employees;
+            employees.RemoveRange(employeesToRemove);
+
+            context.Projects.Remove(project);
+
+            context.SaveChanges();
+
+            var projects = context.Projects.Take(10);
+            foreach (var proj in projects)
+            {
+                Console.WriteLine($"{proj.Name}");
+            }
+        }
+    }
+}
