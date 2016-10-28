a2e4cb04a190608a2426d9e246aca9fe31235502
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-28 11:19:21 +0300

    Add files via upload

---
+++ IntroductionToEntityFramework/19-FirstLetter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Gringotts;
+
+class FirstLetter
+{
+    static void Main(string[] args)
+    {
+        using (var context = new GringottsContext())
+        {
+            var deposits = context.WizzardDeposits
+                .Where(w => w.DepositGroup == "Troll Chest")
+                .OrderBy(w => w.FirstName)
+                .Select(t => new
+                {
+                    FirstLetter = t.FirstName.Substring(0, 1)
+                });
+
+            foreach (var wizzardDeposit in deposits.Distinct())
+            {
+                Console.WriteLine($"{wizzardDeposit.FirstLetter}");
+            }
+        }
+    }
+}
