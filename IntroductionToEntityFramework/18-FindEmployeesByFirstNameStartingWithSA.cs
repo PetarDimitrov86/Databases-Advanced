f3d7d57be24b710eabef7b7fb8a5f1839bad4cad
Petar Dimitrov <lbmikesmith@gmail.com>
2016-10-28 11:10:58 +0300

    Add files via upload

---
+++ IntroductionToEntityFramework/18-FindEmployeesByFirstNameStartingWithSA.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using testing1;
+
+class IncreaseSalaries
+{
+    static void Main(string[] args)
+    {
+        using (var context = new SoftUniContext())
+        {
+            var employees = context.Employees
+                                   .Where(e => e.FirstName.StartsWith("Sa"));
+            
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName} - {employee.JobTitle} - (${employee.Salary})");
+            }
+            //context.SaveChanges();
+        }
+    }
+}
