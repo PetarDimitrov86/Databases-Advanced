2f306e85e3f32a97331bf0ed5d1eca1e555f619c
Petar Dimitrov <lbmikesmith@gmail.com>
2016-11-11 20:49:42 +0200

    Add files via upload

---
+++ EntityFrameworkCodeFirst/02-CreateUser/Models/User.cs
@@ -0,0 +1,37 @@
+﻿using _02_User.Validations;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace _02_User.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(4), MaxLength(30)]
+        public string Username { get; set; }
+
+        [Required]
+        [PasswordValidation]
+        [MinLength(6), MaxLength(50)]
+        public string Password { get; set; }
+
+        [Required]
+        [EmailValidation]
+        public string Email { get; set; }
+
+        [MaxLength(1024)]
+        public byte[] ProfilePicture { get; set; }
+
+        public DateTime? RegisteredOn { get; set; }
+
+        public DateTime? LastTimeLoggedIn { get; set; }
+
+        [Range(1, 120)]
+        public int? Age { get; set; }
+
+        public bool isDeleted { get; set; }
+    }
+}
