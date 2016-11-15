52f9e4f5ab1cd80d6859a38b278a059b3e4ca018
Petar Dimitrov <lbmikesmith@gmail.com>
2016-11-15 16:11:20 +0200

    Add files via upload

---
+++ EntityFrameworkCodeFirst/06-UserTowns/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace _02_CreateUser.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<_02_User.UsersContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "_02_User.UsersContext";
+        }
+
+        protected override void Seed(_02_User.UsersContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
---
+++ EntityFrameworkCodeFirst/06-UserTowns/Models/Town.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _02_CreateUser.Models
+{
+    public class Town
+    {
+        [Key]
+        [MinLength(3), MaxLength(50)]
+        public string Name { get; set; }
+
+        [MinLength(3), MaxLength(50)]
+        public string Country { get; set; }
+    }
+}
