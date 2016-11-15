d94842f43fe9ce7d7c066a122fa230944e025c6d
Petar Dimitrov <lbmikesmith@gmail.com>
2016-11-15 17:09:15 +0200

    Add files via upload

---
+++ EntityFrameworkCodeFirst/11-GetUsersByEmailProvider.cs
@@ -0,0 +1,26 @@
+﻿using _02_User.Models;
+using System;
+using System.Data.Entity.Validation;
+using System.Linq;
+
+namespace _02_User
+{
+    class UsersDb
+    {
+        static void Main(string[] args)
+        {
+            string desiredEmailProvider = Console.ReadLine();
+            UsersContext context = new UsersContext();
+            var result = context.Users.Where(u => u.Email.EndsWith("@" + desiredEmailProvider)).Select(u => new
+            {
+                UserName = u.Username,
+                Email = u.Email
+            }).ToList();
+
+            foreach (var user in result)
+            {
+                Console.WriteLine($"{user.UserName} {user.Email}");
+            }
+        }
+    }
+}
