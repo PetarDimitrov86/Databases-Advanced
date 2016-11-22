0e764321c5a96af034267ff103f380674d3850c8
Petar Dimitrov <lbmikesmith@gmail.com>
2016-11-22 16:20:26 +0200

    Add files via upload

---
+++ PracticesAndArchitecture/RepositoryPatternExample/IO/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace PhotoShare.Client.IO
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        void Add(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        void DeleteRange(IEnumerable<TEntity> entities);
+
+        TEntity GetById(int id);
+
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+    }
+}
