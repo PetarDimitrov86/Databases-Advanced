ed56e868fdb464ef6795c9ed37aef4b41bc8c404
Petar Dimitrov <lbmikesmith@gmail.com>
2016-11-27 14:21:55 +0200

    Add files via upload

---
+++ BestPracticesAndArchitecture/PhotoShare.Data/Mocks/MockRepository.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+
+namespace PhotoShare.Data.Mocks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+    using Interfaces;
+
+
+    public class MockRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private List<TEntity> entities;
+
+        public MockRepository()
+        {
+            this.entities = new List<TEntity>();
+        }
+
+        public void Add(TEntity entity)
+        {
+            this.entities.Add(entity);
+        }
+
+        public void AddRange(ICollection<TEntity> entities)
+        {
+            this.entities.AddRange(entities);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            this.entities.Remove(entity);
+        }
+
+        public void DeleteRange(ICollection<TEntity> entitiesOut)
+        {
+            foreach (var entity in entitiesOut)
+            {
+                this.Delete(entity);
+            }
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return this.entities;
+        }
+
+        public IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate)
+        {
+            return this.entities.Where(predicate.Compile());
+        }
+
+        public TEntity FirstOrDefault()
+        {
+            return this.entities.FirstOrDefault();
+        }
+
+        public TEntity FirstOrDefaultWhere(Expression<Func<TEntity, bool>> predicate)
+        {
+            return this.entities.FirstOrDefault(predicate.Compile());
+        }
+    }
+}
---
+++ BestPracticesAndArchitecture/PhotoShare.Data/Mocks/MockUnitOfWork.cs
@@ -0,0 +1,67 @@
+﻿using PhotoShare.Data.Interfaces;
+using PhotoShare.Models;
+
+namespace PhotoShare.Data.Mocks
+{
+    public class MockUnitOfWork : IUnitOfWork
+    {
+        private IRepository<Album> albums;
+        private IRepository<AlbumRole> albumRoles;
+        private IRepository<Picture> pictures;
+        private IRepository<Tag> tags;
+        private IRepository<Town> towns;
+        private IRepository<User> users;
+
+        public IRepository<Album> Albums
+        {
+            get
+            {
+                return this.albums ?? (this.albums = new MockRepository<Album>());
+            }
+        }
+
+        public IRepository<AlbumRole> AlbumRoles
+        {
+            get
+            {
+                return this.albumRoles ?? (this.albumRoles = new MockRepository<AlbumRole>());
+            }
+        }
+
+        public IRepository<Picture> Pictures
+        {
+            get
+            {
+                return this.pictures ?? (this.pictures = new MockRepository<Picture>());
+            }
+        }
+
+        public IRepository<Tag> Tags
+        {
+            get
+            {
+                return this.tags ?? (this.tags = new MockRepository<Tag>());
+            }
+        }
+
+        public IRepository<Town> Towns
+        {
+            get
+            {
+                return this.towns ?? (this.towns = new MockRepository<Town>());
+            }
+        }
+
+        public IRepository<User> Users
+        {
+            get
+            {
+                return this.users ?? (this.users = new MockRepository<User>());
+            }
+        }
+
+        public void Commit()
+        {
+        }
+    }
+}
